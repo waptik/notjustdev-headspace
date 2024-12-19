da117965bea8d3750042f04974e17c3913f074b6
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-19 12:29:52 +0000

    setup expo-router + basic  root layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Slot, Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={{ title: "Meditations" }}/>
+    </Stack>
+  )
+}
