50a4a53c4c7b14db9238ad08a68865d1498caab8
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-19 12:50:33 +0000

    setup nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Slot, Stack } from "expo-router";
 
+
+// Import your global CSS file
+import "../../global.css";
+
 export default function RootLayout() {
   return (
     <Stack>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,6 @@ import { Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <Text>Home screen</Text>
+    <Text className="font-bold text-4xl p-10 bg-red-500 text-gray-500">Home screen</Text>
   )
 }
