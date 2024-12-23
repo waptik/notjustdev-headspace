a9785e310b7aa76612446acddc828cacb23a90e8
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-23 13:15:42 +0000

    setup in-app purchases/paywall

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,24 @@
 import { Slot, Stack } from "expo-router";
+import Purchases from "react-native-purchases";
 
 // Import your global CSS file
 import "../../global.css";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+
+const apiKey = Platform.select({
+	ios: process.env.EXPO_PUBLIC_REVENUE_CAT_APPLE_KEY,
+	android: process.env.EXPO_PUBLIC_REVENUE_CAT_ANDROID_KEY,
+});
 
 export default function RootLayout() {
+	// Initialize RevenueCat
+	useEffect(() => {
+		Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG);
+
+		Purchases.configure({ apiKey });
+	}, []);
+
 	return (
 		<Stack>
 			<Stack.Screen name="index" options={{ title: "Meditations" }} />
--- src/components/MediationListItem.tsx
+++ src/components/MediationListItem.tsx
@@ -2,25 +2,45 @@ import { Pressable, Text, View } from "react-native";
 import type { Meditation } from "@/types";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import { FontAwesome } from "@expo/vector-icons";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import Paywall, { PAYWALL_RESULT } from "react-native-purchases-ui";
 
 export function MediationListItem({ meditation }: { meditation: Meditation }) {
+	const onPress = async () => {
+		if (!meditation.pro) {
+			router.push(`/meditation/${meditation.id}`);
+			return;
+		}
+
+		const paywallResult = await Paywall.presentPaywallIfNeeded({
+			requiredEntitlementIdentifier: "Pro",
+		});
+
+		console.log("Paywall result", paywallResult);
+
+		switch (paywallResult) {
+			case PAYWALL_RESULT.PURCHASED:
+			case PAYWALL_RESULT.NOT_PRESENTED:
+			case PAYWALL_RESULT.RESTORED:
+				router.push(`/meditation/${meditation.id}`);
+				break;
+
+			default:
+				break;
+		}
+	};
 	return (
-		<Link href={`/meditation/${meditation.id}`} asChild>
-			<Pressable className="flex flex-row gap-5 items-center">
-				<View className="bg-green-700 p-2 rounded-full">
-					<FontAwesome name="check" size={16} color="white" />
+		<Pressable onPress={onPress} className="flex flex-row gap-5 items-center">
+			<View className="bg-green-700 p-2 rounded-full">
+				<FontAwesome name="check" size={16} color="white" />
+			</View>
+			<View className="flex-1 p-5 py-8 border-2 border-gray-300 rounded-2xl">
+				<Text className="font-semibold text-2xl mb-2">{meditation.title}</Text>
+				<View className="flex flex-row items-center gap-2">
+					<FontAwesome6 name="clock" size={16} color="gray" />
+					<Text className="text-gray-600">{meditation.duration} min</Text>
 				</View>
-				<View className="flex-1 p-5 py-8 border-2 border-gray-300 rounded-2xl">
-					<Text className="font-semibold text-2xl mb-2">
-						{meditation.title}
-					</Text>
-					<View className="flex flex-row items-center gap-2">
-						<FontAwesome6 name="clock" size={16} color="gray" />
-						<Text className="text-gray-600">{meditation.duration} min</Text>
-					</View>
-				</View>
-			</Pressable>
-		</Link>
+			</View>
+		</Pressable>
 	);
 }
