e3bd8c30a52ed062aa9e5f91a4cb4b697268c5e1
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-23 13:11:14 +0000

    revert back to using `expo-audio`

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -6,76 +6,19 @@ import {
 	MaterialIcons,
 } from "@expo/vector-icons";
 import Slider from "@react-native-community/slider";
-import {
-	Audio,
-	type AVPlaybackStatus,
-	type AVPlaybackStatusSuccess,
-} from "expo-av";
 import { router, useLocalSearchParams } from "expo-router";
 import { Pressable, SafeAreaView, Text, View } from "react-native";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 import audio1 from "@assets/meditations/audio1.mp3";
-import { useEffect, useState } from "react";
-
-/**
- *
- * TODO:
- * 1. Wait for apple dev account enrollment email status
- * 2. Build the app locally in order to fix `Cannot find module 'ExpoAudio'` error message
- * 3. Revert back to using `expo-audio` package
- */
 
 export default function MediationDetails() {
 	const { id } = useLocalSearchParams<{ id: string }>();
-	const [sound, setSound] = useState<Audio.Sound>(() => new Audio.Sound());
-	const [status, setStatus] = useState<AVPlaybackStatusSuccess | undefined>();
-	const [isPlaying, setIsPlaying] = useState(false);
-	const [error, setError] = useState<string | undefined>(undefined);
+	const player = useAudioPlayer(audio1);
+	const status = useAudioPlayerStatus(player);
 	const mediation = meditations.find((m) => m.id === Number(id));
-	const duration = Number(status?.durationMillis || 0);
-	const currentTime = Number(status?.positionMillis || 0);
-
-	const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
-		if (!status.isLoaded) {
-			setError(status.error);
-			return;
-		}
-
-		setIsPlaying(status.isPlaying);
-		setStatus(status);
-	};
-
-	const onPlayPause = async () => {
-		console.log(`[onPlayPause] >> sound: ${sound ? "found" : "not found"}`);
-
-		console.log("[onPlayPause] >>", isPlaying);
-
-		if (!sound) {
-			return;
-		}
-		if (isPlaying) {
-			await sound.pauseAsync();
-		} else {
-			await sound.playAsync();
-		}
-	};
-
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		const playSound = async () => {
-			const { sound } = await Audio.Sound.createAsync(audio1);
-			setSound(sound);
-
-			sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
-		};
-
-		playSound();
-	}, []);
-
-	useEffect(() => {
-		console.log("[useEffect] >> Unloading sound");
-		sound ? () => sound.unloadAsync() : undefined;
-	}, [sound]);
+	const duration = status.duration;
+	const currentTime = status.currentTime;
 
 	const formatSeconds = (milliseconds: number) => {
 		const totalSeconds = Math.floor(milliseconds / 1000);
@@ -104,8 +47,7 @@ export default function MediationDetails() {
 						</View>
 
 						<AntDesign
-							onPress={async () => {
-								await sound.unloadAsync();
+							onPress={() => {
 								router.back();
 							}}
 							name="close"
@@ -117,19 +59,15 @@ export default function MediationDetails() {
 					<Text className="text-3xl mt-10 text-center text-zinc-800 font-semibold">
 						{mediation.title}: ({mediation.duration} min)
 					</Text>
-					<Text className="text-center text-zinc-800">
-						Current time: {formatSeconds(currentTime)}, playing:{" "}
-						{isPlaying ? "Yes" : "No"}
-					</Text>
 				</View>
 
 				{/* Play/Pause button */}
 				<Pressable
-					onPress={onPlayPause}
+					onPress={() => (player.playing ? player.pause() : player.play())}
 					className="bg-zinc-800 self-center w-20 aspect-square rounded-full items-center justify-center"
 				>
 					<FontAwesome6
-						name={isPlaying ? "pause" : "play"}
+						name={status.playing ? "pause" : "play"}
 						size={24}
 						color="snow"
 					/>
@@ -151,10 +89,10 @@ export default function MediationDetails() {
 						{/* Playback slider */}
 						<Slider
 							onSlidingComplete={(value) => {
-								sound.setPositionAsync(value * Number(duration));
+								player.seekTo(value * duration);
 							}}
 							style={{ height: 3, width: "100%" }}
-							value={Number(currentTime) / Number(duration)}
+							value={currentTime / duration}
 							minimumValue={0}
 							maximumValue={1}
 							minimumTrackTintColor="#3A3937"
@@ -164,13 +102,10 @@ export default function MediationDetails() {
 						{/* Timestamps-Duration: */}
 						<View className="flex-row  justify-between">
 							<Text className="text-zinc-800">
-								{formatSeconds(Number(currentTime))}
+								{formatSeconds(currentTime)}
 							</Text>
-							<Text className="text-zinc-800">
-								{formatSeconds(Number(duration))}
-							</Text>
+							<Text className="text-zinc-800">{formatSeconds(duration)}</Text>
 						</View>
-						{/* TODO: resume from where you left: -1:40:30 */}
 					</View>
 				</View>
 			</View>
